e946de841c071808a0c654d709ee530dc03c15bf
haolinyao367mango <haolinyao3@gmail.com>
2025-11-06 14:39:07 -0800

    Add files via upload

---
+++ js_demo/js/script.js
@@ -0,0 +1,6 @@
+function askQuestioni(){
+    var p=prompt('how much would a woodchuck chuck if a woodchuck could chuck wood?');
+    if (p !=null) {
+        document.getElementById ('question').innerHTML='CHICKEN STARS! ' + p +'ts is a lot of chicken stars!';
+    }
+}
